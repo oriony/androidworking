b33a1e6ef51d59f7d59c468033b7ffb952b34d5d
Orion Yuen <orionyuen.ubc@gmail.com>
2020-02-21 22:29:44 -0500

    Initial datetime displays

---
+++ app/src/main/java/com/example/basic/DateTimeText.java
@@ -0,0 +1,64 @@
+package com.example.basic;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class DateTimeText {
+    static final String DATE_FORMAT = "yyyy-MM-dd";
+    static final String TIME_FORMAT = "kkmm";
+
+    public static Date getUTCdatetimeAsDate() {
+        // note: doesn't check for null
+        return stringDateToDate(getUTCDateAsString());
+    }
+
+    public static String getUTCDateAsString() {
+        final SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT);
+        sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+        final String utcDate = sdf.format(new Date());
+
+        return utcDate;
+    }
+
+    public static String getDateAsString() {
+        final SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT);
+        final String lclDate = sdf.format(new Date());
+
+        return lclDate;
+    }
+
+    public static String getUTCTimeAsString() {
+        final SimpleDateFormat sdf = new SimpleDateFormat(TIME_FORMAT);
+        sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+        final String utcTime = sdf.format(new Date());
+
+        return utcTime;
+    }
+
+    public static String getTimeAsString() {
+        final SimpleDateFormat sdf = new SimpleDateFormat(TIME_FORMAT);
+        final String lclTime = sdf.format(new Date());
+
+        return lclTime;
+    }
+
+
+
+    public static Date stringDateToDate(String StrDate) {
+        Date dateToReturn = null;
+        SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
+
+        try {
+            dateToReturn = (Date)dateFormat.parse(StrDate);
+        }
+        catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return dateToReturn;
+    }
+}
+
+
---
+++ app/src/main/java/com/example/basic/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.basic;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.provider.BaseColumns;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.example.basic.DateTimeText.getDateAsString;
+import static com.example.basic.DateTimeText.getTimeAsString;
+import static com.example.basic.DateTimeText.getUTCDateAsString;
+import static com.example.basic.DateTimeText.getUTCTimeAsString;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TextView lclTime = (TextView)findViewById(R.id.lclTime);
+        lclTime.setText(getTimeAsString());
+        TextView utcTime = (TextView)findViewById(R.id.utcTime);
+        utcTime.setText(getUTCTimeAsString());
+
+        TextView lclDate = (TextView)findViewById(R.id.lclDate);
+        lclDate.setText(getDateAsString());
+        TextView utcDate = (TextView)findViewById(R.id.utcDate);
+        utcDate.setText(getUTCDateAsString());
+
+        final FeedReaderDBHelper dbHelper = new FeedReaderDBHelper(getApplicationContext());
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, getDateAsString(), Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
